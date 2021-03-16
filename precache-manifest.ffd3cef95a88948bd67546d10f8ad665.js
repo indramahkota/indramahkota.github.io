217aa38593487724c82503f09077e2f98ea6812b
Indra Mahkota <indramahkota1@gmail.com>
2021-03-16 19:58:54 +0700

    first commit

---
+++ precache-manifest.ffd3cef95a88948bd67546d10f8ad665.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce9146ae2087c8fe0462d8d1ab2065d4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "789e8c6cbabae3e475e6",
+    "url": "/static/css/main.901cf3c1.chunk.css"
+  },
+  {
+    "revision": "b848fb7dd867c178815d",
+    "url": "/static/js/2.6f7bc242.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.6f7bc242.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "789e8c6cbabae3e475e6",
+    "url": "/static/js/main.00d5e171.chunk.js"
+  },
+  {
+    "revision": "55b58af4b82fb2fb439d",
+    "url": "/static/js/runtime-main.3a591e9e.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/static/media/manOnTable.52be3144.svg"
+  }
+]);
